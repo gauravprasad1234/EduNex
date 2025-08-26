56074ffead32aef5fd8e6008713f5d067f31bcdd
priyanjal1234 <priyanjal362@gmail.com>
2025-08-26 10:54:52 +0530

    Update Hero.jsx

--- client/src/components/student/Hero.jsx
+++ client/src/components/student/Hero.jsx
@@ -5,7 +5,8 @@ import SearchBar from './SearchBar'
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-full md:pt-36 pt-20 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+    <div className='flex flex-col items-center justify-center w-full pt-0 px-7 md:px-0 space-y-7 text-center bg-gradient-to-b from-cyan-100/70'>
+
       <h1 className='md:text-home-heading-large text-home-heading-small font-bold text-gray-800 max-w-3xl mx-auto relative'>
         Empower your future with the courses designed to{' '}
         <span className="relative inline-block text-blue-600">
